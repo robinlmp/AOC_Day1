b55a3e1f6fbf2eab3f713b14509bbc0f9ba394a1
Robin <robinlmp@mac.com>
2021-12-01 20:19:27 +0000

    More refinements

--- AOC_Day1/ContentView.swift
+++ AOC_Day1/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("number of increases: \(countIncreases(readings: rollingTotals(readings: loadFile(fileName: "input") ?? [] )))")
+        VStack {
+            Text("number of increases (single): \(countIncreases(readings: loadFile(fileName: "input") ?? [] ))")
+            Text("number of increases (rolling): \(countIncreases(readings: rollingTotals(readings: loadFile(fileName: "input") ?? [] )))")
+        }
     }
     
     
@@ -24,19 +27,16 @@ struct ContentView: View {
     
     func countIncreases(readings: [Int]) -> Int {
         var counter = 0
-        for i in 0..<readings.count where i > 0 && readings[i] > readings[i-1] {
-                counter += 1
-        }
+        for i in 0..<readings.count where i > 0 && readings[i] > readings[i-1] { counter += 1 }
         return counter
     }
     
     
     func loadFile(fileName: String) -> [Int]? {
-        guard let filepath = Bundle.main.path(forResource: fileName, ofType: "txt"), let contents = try? String(contentsOfFile: filepath) else { return nil }
+        guard let filepath = Bundle.main.path(forResource: fileName, ofType: "txt"),
+              let contents = try? String(contentsOfFile: filepath) else { return nil }
         
-        let stringArray = contents.components(separatedBy: "\n")
-        let returnArray = stringArray.compactMap(Int.init)
-
+        let returnArray = contents.components(separatedBy: "\n").compactMap(Int.init)
         return returnArray
     }
 }
