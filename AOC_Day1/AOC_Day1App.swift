f8f7d13cfafdbdec1c32089ea5c36063b22a7529
Robin <robinlmp@mac.com>
2021-12-01 15:54:12 +0000

    Initial Commit

---
+++ AOC_Day1/AOC_Day1App.swift
@@ -0,0 +1,17 @@
+//
+//  AOC_Day1App.swift
+//  AOC_Day1
+//
+//  Created by Robin Phillips on 01/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AOC_Day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
