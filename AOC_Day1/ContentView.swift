2c22569eab8cad9502dde88721ea32b99b11eb58
Robin <robinlmp@mac.com>
2021-12-01 20:15:33 +0000

    Simplified code dramatically. Many of the improvements thanks to Oliver Binns

--- AOC_Day1/ContentView.swift
+++ AOC_Day1/ContentView.swift
@@ -9,58 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("number of increases: \(countIncreases(readings: rollingTotals(readings: loadFile(fileName: "input")) ))")
+        Text("number of increases: \(countIncreases(readings: rollingTotals(readings: loadFile(fileName: "input") ?? [] )))")
     }
     
     
     func rollingTotals(readings: [Int]) -> [Int] {
-        var returnArray: [Int] = []
-        for i in 0..<readings.count {
-            if i < 2 {
-                continue
-            } else {
-                returnArray.append( readings[i-2] + readings[i-1] + readings[i] )
-            }
+        var rollingTotals: [Int] = []
+        for i in 0..<readings.count where i > 1 {
+                rollingTotals.append( readings[i-2] + readings[i-1] + readings[i] )
         }
-        return returnArray
+        return rollingTotals
     }
     
     
     func countIncreases(readings: [Int]) -> Int {
         var counter = 0
-        for i in 0..<readings.count {
-            if i == 0 {
-                continue
-            } else {
-                if readings[i] > readings[i-1] {
-                    counter += 1
-                }
-            }
+        for i in 0..<readings.count where i > 0 && readings[i] > readings[i-1] {
+                counter += 1
         }
         return counter
     }
     
     
-    func loadFile(fileName: String) -> [Int] {
-        if let filepath = Bundle.main.path(forResource: fileName, ofType: "txt") {
-            do {
-                var returnArray: [Int] = []
-                
-                let contents = try String(contentsOfFile: filepath)
-                let stringArray = contents.components(separatedBy: "\n")
-                
-                for i in 0..<stringArray.count - 1 {
-                    returnArray.append(Int(stringArray[i]) ?? 0 )
-                }
-                return returnArray
-                
-            } catch {
-                // contents could not be loaded
-            }
-        } else {
-            // example.txt not found!
-        }
-        return [9999] // defaut return value if everything above fails
+    func loadFile(fileName: String) -> [Int]? {
+        guard let filepath = Bundle.main.path(forResource: fileName, ofType: "txt"), let contents = try? String(contentsOfFile: filepath) else { return nil }
+        
+        let stringArray = contents.components(separatedBy: "\n")
+        let returnArray = stringArray.compactMap(Int.init)
+
+        return returnArray
     }
 }
 
